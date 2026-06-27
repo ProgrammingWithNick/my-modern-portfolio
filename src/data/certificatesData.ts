8172564d56fa160ac11aa3ff1ed44ed7752832f6
Nikhil <nikhilkhavdu441@gmail.com>
2026-06-27 19:11:05 +0530

    commit

--- src/data/certificatesData.ts
+++ src/data/certificatesData.ts
@@ -1,15 +1,27 @@
 import type { Certificate } from '../types';
 
 // Import your certificate images directly from your source files
-import awsCertImage from '../assets/certificates/aws.jpeg';
+import ai from '../assets/certificates/ai.jpeg';
+import oracle from '../assets/certificates/oracle.png';
 
 export const certificatesData: Certificate[] = [
   {
     id: 'cert-1',
-    title: 'AWS Certified Cloud Practitioner',
-    organization: 'Amazon Web Services',
-    date: 'Jan 2026',
-    image: awsCertImage, // Using the imported variable here
-    downloadPath: awsCertImage, // Using the imported variable here
-  }
+    title: 'Artificial Intelligence Certification',
+    organization: 'Elewayte',
+    date: 'May-2026 to June-2026',
+    image: ai, // Using the imported variable here
+    downloadPath: ai, // Using the imported variable here
+  },
+  {
+    id: 'cert-2',
+    title: 'Database Programming With SQL Certification',
+    organization: 'Oracle',
+    date: 'Nov-2025',
+    image: oracle, // Using the imported variable here
+    downloadPath: oracle, // Using the imported variable here
+  },
+
+
+
 ];
--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,16 +1,28 @@
 import type { Project } from '../types';
 
 
-import awsCertImage from '../assets/certificates/aws.jpeg';
+import netflix from '../assets/projects/netflix.png';
+import ms from '../assets/projects/ms.png';
 
 export const projectsData: Project[] = [
   {
     id: '1',
-    title: 'Enterprise FinTech Core Platform',
-    description: 'High-throughput, real-time asset ledger processing engine optimized for multi-currency transactions.',
-    image: awsCertImage, // Using the imported variable here
-    techStack: ['React 19', 'TypeScript', 'NestJS', 'PostgreSQL', 'Docker'],
-    features: ['Sub-10ms transactional syncing Engine', 'End-to-end Ledger cryptographical audits', 'Custom declarative UI schemas'],
-    githubUrl: 'https://github.com/ProgrammingWithNick',
-  }
+    title: ' Netflix Clone — Built with React, TypeScript & Vite',
+    description: 'A stunning Netflix Clone made with ⚡ Vite, 💙 React, and 💡 TypeScript — featuring cinematic UI animations, live search, autoplay trailers, and more.',
+    image: netflix, // Using the imported variable here
+    techStack: ['Vite', 'React 19', 'TypeScript'],
+    features: ['Cinematic UI Animations', 'Live Search', 'Autoplay Trailers', 'Responsive Design'],
+    githubUrl: 'https://github.com/ProgrammingWithNick/Netflix',
+  },
+
+  {
+    id: '2',
+    title: 'Mobile Shop — Built with Java, Android Studio & Firebase',
+    description: 'Mobile Shop is a feature-rich e-commerce platform designed to provide seamless product management and shopping experiences.',
+    image: ms, // Using the imported variable here
+    techStack: ['Java', 'Android Studio', 'Firebase Console'],
+    features: ['Cinematic UI Animations', 'Live Search', 'Autoplay Trailers', 'Responsive Design'],
+    githubUrl: 'https://github.com/ProgrammingWithNick/MobileShop-With-AdminSide',
+  },
+
 ];
