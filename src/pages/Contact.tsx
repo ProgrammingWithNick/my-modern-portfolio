260434999c71966c74af331573eb27ef24d73184
Nikhil <nikhilkhavdu441@gmail.com>
2026-06-27 19:20:54 +0530

    last fix contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { Mail, Phone, MapPin, Send } from "lucide-react";
+import { Mail, MapPin, Send } from "lucide-react";
 import { PageTransition } from "../components/PageTransition";
 import { SEO } from "../components/SEO";
 
